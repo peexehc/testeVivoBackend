bc75c5227e6cbaceba6484193352471c69c2c6e2
Douglas Garbim Jr <garbim.douglas@gmail.com>
2019-09-09 21:19:06 -0300

    Championship API team filter enabled

--- src/Controllers/Api.js
+++ src/Controllers/Api.js
@@ -12,7 +12,8 @@ module.exports = {
       t1.nomeTime as timeMandante, 
       t2.nomeTime as timeVisitante, 
       tt_rodadas.placarTimeA as placarTimeMandante, 
-      tt_rodadas.placarTimeB as placarTimeVisitante 
+      tt_rodadas.placarTimeB as placarTimeVisitante,
+      CONCAT(tt_rodadas.placarTimeA, 'x', tt_rodadas.placarTimeB) as resultadoPartida
       FROM tt_rodadas 
       INNER JOIN tt_times t1 ON tt_rodadas.codigoTimeA = t1.codigoTime
       INNER JOIN tt_times t2 ON tt_rodadas.codigoTimeB = t2.codigoTime
@@ -45,9 +46,77 @@ module.exports = {
         if (req.params.rodada && response[req.params.rodada] !== undefined) {
           return res.json(response[req.params.rodada])
         } else if (req.params.rodada && response[req.params.rodada] === undefined) {
-          return res.send(`Rodada do campeonato inválida!<br/><br/>Rodadas disponíveis: ${rodadas}`)
+          return res.status(400).send(`Rodada do campeonato inválida!<br/><br/>Rodadas disponíveis: ${rodadas}`)
         } else {
           return res.json(response)
+        }
+      })
+    })
+  },
+
+  championshipByTeam (req, res) {
+    pool.getConnection((error, connection) => {
+      if (error) console.log(error)
+
+      const queryTimes = `SELECT
+      nomeTime as time
+      FROM tt_times
+      ORDER BY nomeTime ASC`
+
+      const times = []
+
+      connection.query(queryTimes, (error, result) => {
+        if (error) console.log(error)
+
+        result.forEach(time => {
+          times.push(time.time)
+        })
+      })
+
+      const queryJogos = `SELECT 
+      tt_rodadas.codigoRodada as rodada,
+      tt_rodadas.dataJogo as data, 
+      tt_rodadas.horaJogo as hora, 
+      t1.nomeTime as timeMandante, 
+      t2.nomeTime as timeVisitante, 
+      tt_rodadas.placarTimeA as placarTimeMandante, 
+      tt_rodadas.placarTimeB as placarTimeVisitante,
+      CONCAT(tt_rodadas.placarTimeA, 'x', tt_rodadas.placarTimeB) as resultadoPartida
+      FROM tt_rodadas 
+      INNER JOIN tt_times t1 ON tt_rodadas.codigoTimeA = t1.codigoTime
+      INNER JOIN tt_times t2 ON tt_rodadas.codigoTimeB = t2.codigoTime
+      WHERE LOWER(t1.nomeTime) LIKE '%${req.params.time.toLowerCase()}%'
+      OR LOWER(t2.nomeTime) LIKE '%${req.params.time.toLowerCase()}%'
+      ORDER BY dataJogo, horaJogo ASC`
+
+      connection.query(queryJogos, (error, result) => {
+        connection.release()
+        const response = {}
+
+        if (error) console.log(error)
+
+        if (result.length >= 1) {
+          const rodadas = [...new Set(result.map(item => item.rodada))]
+
+          rodadas.forEach(rodada => {
+            Object.defineProperty(response, rodada, {
+              value: {
+                rodada: rodada,
+                qtdJogos: '',
+                jogos: []
+              },
+              enumerable: true
+            })
+          })
+
+          result.forEach(jogo => {
+            response[jogo.rodada].jogos.push(jogo)
+            response[jogo.rodada].qtdJogos = response[jogo.rodada].jogos.length
+          })
+
+          return res.json(response)
+        } else {
+          return res.status(400).send(`Time não encontrado!<br/><br/>Times disponíveis: ${times}`)
         }
       })
     })
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const Api = require('./Controllers/Api')
 
 const routes = new express.Router()
 
-routes.get('/championship/todos-jogos', Api.championship)
-routes.get('/championship/rodada/:rodada', Api.championship)
+routes.get('/championship/:rodada?', Api.championship)
+routes.get('/championship/time/:time', Api.championshipByTeam)
 
 module.exports = routes
