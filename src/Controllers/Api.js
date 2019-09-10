f38b0b729c0ca666f962e0328cfc179b89136e77
Douglas Garbim Jr <garbim.douglas@gmail.com>
2019-09-10 01:11:30 -0300

    Enabled team API with team and round filters

--- src/Controllers/Api.js
+++ src/Controllers/Api.js
@@ -34,7 +34,8 @@ module.exports = {
               qtdJogos: '',
               jogos: []
             },
-            enumerable: true
+            enumerable: true,
+            configurable: true
           })
         })
 
@@ -105,7 +106,8 @@ module.exports = {
                 qtdJogos: '',
                 jogos: []
               },
-              enumerable: true
+              enumerable: true,
+              configurable: true
             })
           })
 
@@ -118,6 +120,213 @@ module.exports = {
         } else {
           return res.status(400).send(`Time não encontrado!<br/><br/>Times disponíveis: ${times}`)
         }
+      })
+    })
+  },
+
+  team (req, res) {
+    pool.getConnection((error, connection) => {
+      if (error) console.log(error)
+
+      const queryTimes = `SELECT
+      nomeTime as time
+      FROM tt_times
+      ORDER BY nomeTime ASC`
+
+      const times = []
+
+      connection.query(queryTimes, (error, result) => {
+        if (error) console.log(error)
+
+        result.forEach(time => {
+          times.push(time.time)
+        })
+      })
+
+      const queryJogos = `SELECT
+      tt_rodadas.codigoRodada as rodada,
+      tt_rodadas.dataJogo as data,
+      tt_rodadas.horaJogo as hora,
+      t1.nomeTime as timeMandante,
+      t2.nomeTime as timeVisitante,
+      tt_rodadas.placarTimeA as placarTimeMandante,
+      tt_rodadas.placarTimeB as placarTimeVisitante,
+      CONCAT(tt_rodadas.placarTimeA, 'x', tt_rodadas.placarTimeB) as resultadoPartida
+      FROM tt_rodadas
+      INNER JOIN tt_times t1 ON tt_rodadas.codigoTimeA = t1.codigoTime
+      INNER JOIN tt_times t2 ON tt_rodadas.codigoTimeB = t2.codigoTime
+      ORDER BY t1.nomeTime, t2.nomeTime ASC`
+
+      connection.query(queryJogos, (error, result) => {
+        connection.release()
+        const response = {}
+
+        if (error) console.log(error)
+
+        times.forEach(time => {
+          Object.defineProperty(response, time.replace(/(\r\n|\n|\r)/gm, ''), {
+            value: {
+              time: time.replace(/(\r\n|\n|\r)/gm, ''),
+              qtdJogos: '',
+              qtdVitorias: '',
+              qtdDerrotas: '',
+              qtdEmpates: '',
+              jogos: []
+            },
+            enumerable: true,
+            configurable: true
+          })
+        })
+
+        result.forEach(jogo => {
+          const timeMandante = jogo.timeMandante.replace(/(\r\n|\n|\r)/gm, '')
+          const timeVisitante = jogo.timeVisitante.replace(/(\r\n|\n|\r)/gm, '')
+          let vitoriaMandante = false
+          let vitoriaVisitante = false
+          let empate = false
+
+          if (jogo.placarTimeMandante > jogo.placarTimeVisitante) {
+            vitoriaMandante = true
+          } else if (jogo.placarTimeMandante === jogo.placarTimeVisitante) {
+            empate = true
+          } else {
+            vitoriaVisitante = true
+          }
+
+          response[timeMandante].jogos.push(jogo)
+          response[timeVisitante].jogos.push(jogo)
+
+          response[timeMandante].qtdJogos = response[timeMandante].jogos.length
+          response[timeVisitante].qtdJogos = response[timeVisitante].jogos.length
+
+          if (vitoriaMandante) {
+            response[timeMandante].qtdVitorias++
+            response[timeVisitante].qtdDerrotas++
+          }
+
+          if (vitoriaVisitante) {
+            response[timeVisitante].qtdVitorias++
+            response[timeMandante].qtdDerrotas++
+          }
+
+          if (empate) {
+            response[timeMandante].qtdEmpates++
+            response[timeVisitante].qtdEmpates++
+          }
+        })
+
+        const resArray = Object.values(response)
+
+        if (req.params.time) {
+          const buscaTime = resArray.filter((time) => {
+            return (time.time.toLowerCase().includes(req.params.time.toLowerCase()))
+          })
+
+          if (buscaTime.length < 1) return res.status(400).send(`Time não encontrado!<br/><br/>Times disponíveis: ${times}`)
+
+          return res.json(buscaTime)
+        }
+
+        return res.json(resArray)
+      })
+    })
+  },
+
+  teamByRound (req, res) {
+    pool.getConnection((error, connection) => {
+      if (error) console.log(error)
+
+      const queryTimes = `SELECT
+      nomeTime as time
+      FROM tt_times
+      ORDER BY nomeTime ASC`
+
+      const times = []
+
+      connection.query(queryTimes, (error, result) => {
+        if (error) console.log(error)
+
+        result.forEach(time => {
+          times.push(time.time)
+        })
+      })
+
+      const queryJogos = `SELECT
+      tt_rodadas.codigoRodada as rodada,
+      tt_rodadas.dataJogo as data,
+      tt_rodadas.horaJogo as hora,
+      t1.nomeTime as timeMandante,
+      t2.nomeTime as timeVisitante,
+      tt_rodadas.placarTimeA as placarTimeMandante,
+      tt_rodadas.placarTimeB as placarTimeVisitante,
+      CONCAT(tt_rodadas.placarTimeA, 'x', tt_rodadas.placarTimeB) as resultadoPartida
+      FROM tt_rodadas
+      INNER JOIN tt_times t1 ON tt_rodadas.codigoTimeA = t1.codigoTime
+      INNER JOIN tt_times t2 ON tt_rodadas.codigoTimeB = t2.codigoTime
+      WHERE tt_rodadas.codigoRodada = ${req.params.rodada}
+      ORDER BY t1.nomeTime, t2.nomeTime ASC`
+
+      connection.query(queryJogos, (error, result) => {
+        connection.release()
+        const response = {}
+
+        if (error) console.log(error)
+
+        times.forEach(time => {
+          Object.defineProperty(response, time.replace(/(\r\n|\n|\r)/gm, ''), {
+            value: {
+              time: time.replace(/(\r\n|\n|\r)/gm, ''),
+              qtdJogos: '',
+              qtdVitorias: '',
+              qtdDerrotas: '',
+              qtdEmpates: '',
+              jogos: []
+            },
+            enumerable: true,
+            configurable: true
+          })
+        })
+
+        result.forEach(jogo => {
+          const timeMandante = jogo.timeMandante.replace(/(\r\n|\n|\r)/gm, '')
+          const timeVisitante = jogo.timeVisitante.replace(/(\r\n|\n|\r)/gm, '')
+          let vitoriaMandante = false
+          let vitoriaVisitante = false
+          let empate = false
+
+          if (jogo.placarTimeMandante > jogo.placarTimeVisitante) {
+            vitoriaMandante = true
+          } else if (jogo.placarTimeMandante === jogo.placarTimeVisitante) {
+            empate = true
+          } else {
+            vitoriaVisitante = true
+          }
+
+          response[timeMandante].jogos.push(jogo)
+          response[timeVisitante].jogos.push(jogo)
+
+          response[timeMandante].qtdJogos = response[timeMandante].jogos.length
+          response[timeVisitante].qtdJogos = response[timeVisitante].jogos.length
+
+          if (vitoriaMandante) {
+            response[timeMandante].qtdVitorias++
+            response[timeVisitante].qtdDerrotas++
+          }
+
+          if (vitoriaVisitante) {
+            response[timeVisitante].qtdVitorias++
+            response[timeMandante].qtdDerrotas++
+          }
+
+          if (empate) {
+            response[timeMandante].qtdEmpates++
+            response[timeVisitante].qtdEmpates++
+          }
+        })
+
+        const resArray = Object.values(response)
+
+        return res.json(resArray)
       })
     })
   }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const routes = new express.Router()
 
 routes.get('/championship/:rodada?', Api.championship)
 routes.get('/championship/time/:time', Api.championshipByTeam)
+routes.get('/team/:time?', Api.team)
+routes.get('/team/rodada/:rodada', Api.teamByRound)
 
 module.exports = routes
